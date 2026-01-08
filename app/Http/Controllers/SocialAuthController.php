763530936571e60d8337ef044a4e4b7544f00aa5
Fathma Khatun Mim <fathmakhatunmim@gmail.com>
2026-01-08 12:16:32 +0600

    git authication

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class SocialAuthController extends Controller
 {
@@ -13,7 +16,36 @@ class SocialAuthController extends Controller
     }
 
     public function googleCallback(){
-        $googleUser = Socialite::driver('google')->user();
-        dd($googleUser);
+       
+        try{
+           $googleUser = Socialite::driver('google')->stateless()->user();
+
+
+            
+            $user = user::where('google_id' , $googleUser->id)->first();
+
+            if($user){
+                Auth::login($user);
+                return redirect()->route('dashboard');
+
+            }
+            else{
+                $userData = user::create([
+                    'name' => $googleUser->name,
+                    'email' => $googleUser->email,
+                    'password' => Hash::make('password@1234'),
+                    'google_id' => $googleUser->id,
+
+                ]);
+
+            if($userData){
+                Auth::login($userData);
+                return redirect()->route('dashboard');
+            }
+            }
+        }
+        catch(Exception $e){
+            dd($e);
+        }
     }
 }
